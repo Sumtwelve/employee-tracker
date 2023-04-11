6c832f8113fdf1eb6d475bbf3cebd1ae649c321f
sumtwelve <sumtwelve@gmail.com>
2023-04-10 21:39:43 -0600

    install express, rename main.js to server.js, server.js establishes connection to mysql2

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
+const express = require('express');
+const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    },
+    console.log(`Connected to ${process.env.DB_NAME} database.`)
+);
 
 // wanted to make this somewhat fancy so here's a fancy logo
 // I put it into a separate function to reduce clutter
